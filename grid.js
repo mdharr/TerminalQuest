cbfad72f0a33155106214b82372f98d0bc13216e
Michael Harrington <mdharr7@gmail.com>
2024-01-23 16:38:34 +0800

    Write Grid constructor for building a grid as well as a method for displaying the grid to the console.

--- grid.js
+++ grid.js
@@ -1,5 +1,35 @@
 class Grid {
+    constructor(width, height) {
+        this.width = width
+        this.height = height
 
+        this.grid = []
+        for (let row = 0; row < height; row++) {
+            let thisRow = []
+            for (let col = 0; col < width; col++) {
+                thisRow.push('🌳')
+            }
+            this.grid.push(thisRow)
+        }
+
+        this.grid[height-1][0] = '🧝'
+        this.grid[0][width-1] = '🏰'
+
+        this.displayGrid()
+    }
+
+    displayGrid() {
+        for (let row = 0; row < this.height; row++) {
+            for (let col = 0; col < this.width; col++) {
+                process.stdout.write(`${this.grid[row][col]} `)
+            }
+            process.stdout.write(`\n`)
+        }
+    }
 }
+
+new Grid(15, 15)
+
+
 
 export { Grid }
