6cd9b8a2b0b79f4c89e4ba02322c91614fcc9542
Michael Harrington <mdharr7@gmail.com>
2024-01-25 10:50:20 +0800

    Give player method for getStats which returns a copy of our private stats object to avoid writing over original

--- player.js
+++ player.js
@@ -8,6 +8,13 @@ class Player {
         this.name = name
         this.stats = stats
     }
+    getStats() {
+        return {
+            attack: this.#stats.attack,
+            defense: this.#stats.defense,
+            hp: this.#stats.hp
+        }
+    }
 }
 
 export { Player }
