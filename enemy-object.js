a04d1ffcca2fd6e3881cec30f9c41661b14ec03b
Michael Harrington <mdharr7@gmail.com>
2024-01-25 10:38:03 +0800

    Add private stats field for enemy object

--- enemy-object.js
+++ enemy-object.js
@@ -1,6 +1,12 @@
 import { GridObject } from "./grid-object.js";
 
 class EnemyObject extends GridObject {
+    #stats = {
+        name: null,
+        attack: 0,
+        defense: 0,
+        hp: 0
+    }
     constructor(sprite, stats) {
         super(sprite)
         this.type = "enemy"
