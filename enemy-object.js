3d1ce96d84b11fa9aa6d0ec975c3ec3158eba1bd
Michael Harrington <mdharr7@gmail.com>
2024-01-25 10:37:26 +0800

    Create enemy object class extending from grid object along with constructor

--- enemy-object.js
+++ enemy-object.js
@@ -1,7 +1,11 @@
 import { GridObject } from "./grid-object.js";
 
 class EnemyObject extends GridObject {
-
+    constructor(sprite, stats) {
+        super(sprite)
+        this.type = "enemy"
+        this.#stats = stats
+    }
 }
 
 export { EnemyObject }
