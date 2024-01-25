32c82a6262307aed387ff00c81e66797f609893b
Michael Harrington <mdharr7@gmail.com>
2024-01-25 10:49:03 +0800

    Create player class along with private stats object and constructor

--- player.js
+++ player.js
@@ -1,5 +1,13 @@
 class Player {
-
+    #stats = {
+        attack: 0,
+        defense: 0,
+        hp: 0
+    }
+    constructor(name, stats) {
+        this.name = name
+        this.stats = stats
+    }
 }
 
 export { Player }
