5d3b2831e9de1c0dfee0061c0a903d3dbb6a28b1
Michael Harrington <mdharr7@gmail.com>
2024-01-25 10:41:45 +0800

    Create item object class extending from grid object along with constructor

--- item-object.js
+++ item-object.js
@@ -1,7 +1,11 @@
 import { GridObject } from "./grid-object.js"
 
 class ItemObject extends GridObject {
-
+    constructor(sprite, stats) {
+        super(sprite)
+        this.type = 'item',
+        this.#stats = stats
+    }
 }
 
 export { ItemObject }
